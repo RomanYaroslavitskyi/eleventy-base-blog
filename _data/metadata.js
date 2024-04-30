7e92a65c69d0bdf9ef27cc546230dbd36981c012
Roman <Chugaisters@gmail.com>
2024-04-30 11:46:07 +0100

    Added: post+logo+contacts

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Roman Yaroslavitskyi`s Blog ",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		name: "Roman",
+		email: "roman.yaroslavitskyi@gmail.com",
+		url: "https://example.com/about-me/",
+	},
+};
---
+++ content/contact/index.md
@@ -0,0 +1,10 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: Contact
+  order: 4
+---
+
+# Contact
+
+E-mail: qwertyqwerty@gmail.com
